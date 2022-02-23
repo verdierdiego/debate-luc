591db49e8df12cff8a7ea87361cbe525efb21ac9
verdierdiego <dverdierbemposta@gmail.com>
2022-02-22 21:55:02 -0300

    hora actualizada 21:55:02

--- agrupar.py
+++ agrupar.py
@@ -6,12 +6,14 @@ archivo_hashtags = open('hashtags.csv', 'w')
 archivo_menciones = open('menciones.csv', 'w')
 archivo_cantidad = open('total.csv', 'w')
 archivo_horas = open('horasRefinado.csv', 'w')
+archivo_usuarios = open('usuarios.csv', 'w')
 
 archivo_global.write('identificador,dia,hora,user,texto'+'\n')
 archivo_hashtags.write('hashtag,cantidad'+'\n')
 archivo_menciones.write('usuario,cantidad'+'\n')
 archivo_cantidad.write('tuits,total'+'\n')
 archivo_horas.write('hora,cantidad'+'\n')
+archivo_usuarios.write('usuario,cantidad'+'\n')
 
 hashtags = {}
 primera = True
@@ -48,6 +50,7 @@ for attribute, value in hashtags.items():
 
 tuits = []
 horas = {}
+usuarios = {}
 with open('./../global.csv') as input_file:
   for row in csv.reader(input_file, delimiter=','):
     if primera:
@@ -61,11 +64,18 @@ with open('./../global.csv') as input_file:
           horas[hora] += 1
         except:
           horas[hora] = 0
+        user = row[3]
+        try:
+          usuarios[user] += 1
+        except:
+          usuarios[user] = 0
         archivo_global.write((',').join(row)+'\n')
         tuits.append(identificador)
 
 archivo_cantidad.write('total'+','+str(len(tuits)))
 for attribute, value in horas.items():
   archivo_horas.write(attribute+'0,'+str(value)+'\n')
+for attribute, value in usuarios.items():
+  archivo_usuarios.write(attribute+'0,'+str(value)+'\n')
 
 print('ok')
